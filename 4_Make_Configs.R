9a0465d2cdbeba05eab22b6cc44b45bb6c86f20f
Adam VanIwaarden <avaniwaarden@gmail.com>
2021-07-05 22:27:34 -0600

    First test of branch specific commit

--- 4_Make_Configs.R
+++ 4_Make_Configs.R
@@ -13,91 +13,79 @@
 
 custom.config <- list(
   client.info = list(
-    state.name = "Demonstration COVID", # required at a minimum
-    state.abv = "D.C.", # for cover page, not SGPstateData
-    city.name = "Washington",
-    organization = "Demonstration Department of Education",
-    org.head = "Joseph R. Biden, Jr.",
-    github.repo = "CenterForAssessment/SGP_Research/tree/master/Demonstration/Learning_Loss_Analysis",
-    acknowledgements = "the entire staff of the DDoE Assessment and Accountability Office, and particularly Maggie Q. Public,"
+    state.name = "Colorado",
+    state.abv = "CO",
+    city.name = "Denver",
+    organization = "Colorado Department of Education",
+    org.head = "Commissioner Katy Anthes, Ph.D.",
+    github.repo = "CenterForAssessment/SGP_Research/tree/master/Colorado/Learning_Loss_Analysis",
+    acknowledgements = "the entire staff of the CDE Accountability Analytics Office, particularly Marie Huchton and Josh Perdue,"
   ),
   # Override defaults for author/Affil
-  top.level = list(  #  Title/subtitle, author.names, author.affil, date
-    title = "Example Academic Impact Analysis",
-    subtitle = "Student Achievement and Growth during the COVID-19 Pandemic",
-    draft = TRUE  #  default if TRUE - "DRAFT REPORT -- DO NOT CITE OR CIRCULATE" #
+  top.level = list(             # Title/subtitle, author.names, author.affil, date
+    title = "Student Achievement and Growth during the COVID-19 Pandemic",
+    subtitle = "Academic Impact from a Year of Disruptions",
+    draft = TRUE # default if TRUE - "DRAFT REPORT -- DO NOT CITE OR CIRCULATE" #
   ),
-  ##  `params` are passed to R and executed internally to create Universal/Customized
-  ##  text or used to run further analyses on the Report_Data.  Many of these can
-  ##  be created internally in the `params.Rmd` script. This list is semi-exhaustive
-  ##  of what can be supplied to the .Rmd.
   params = list(
     # draft.text = "ALTERNATE DRAFT TEXT",
-    keyword = "academic impact", # should be lower case.  Camel applied as needed in params.Rmd or can be customized as keyword_camel
-    min.size.school = 15,  #  N size cutoff - exclude SCHOOLs with fewer than X students from summaries/analyses
-    min.size.district = 50, # N size cutoff - exclude DISTRICTs with fewer than X students from summaries/analyses
+    keyword   =  "academic impact", # should be lower case.  Camel applied as needed in params.Rmd or can be customized as keyword_camel
+    min.size.school = 15,
+    min.size.district = 50,
     sgp.abv = list( # SGP package abbreviation for accessing relevant SGPstateData meta-data.
-      State_Assessment = "DEMO_COVID",
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      State_Assessment = "CO",
+      College_Entrance = "CO",
+      ELP_Assessment = "WIDA_CO"
     ),
     years = list(
       State_Assessment = as.character(c(2017:2019, 2021)),
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      College_Entrance = as.character(c(2017:2019, 2021)),
+      ELP_Assessment = as.character(2017:2021)
     ),
     GL_subjects = list(
       State_Assessment = c("ELA", "MATHEMATICS"),
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      College_Entrance = c("ELA_PSAT_9", "ELA_PSAT_10", "ELA_SAT",
+                           "MATHEMATICS_PSAT_9", "MATHEMATICS_PSAT_10", "MATHEMATICS_SAT"),
+      ELP_Assessment = "READING"
     ),
     GL_text = list(
       State_Assessment = "ELA and mathematics",
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      College_Entrance = "PSAT and SAT in ELA and Mathematics", # Colorado School Day PSAT and SAT
+      ELP_Assessment = "Overall ELP"
     ),
     test.name = list(
-      State_Assessment = "Demonstration Student Assessment Program",
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      State_Assessment = "Colorado Measures of Academic Success",
+      College_Entrance = "College Board SAT",
+      ELP_Assessment = "WIDA ACCESS"
     ),
     test.abv = list(
-      State_Assessment = "DEMO_COVID",
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      State_Assessment = "CMAS",
+      College_Entrance = "PSAT/SAT",
+      ELP_Assessment = "ACCESS"
     ),
     test.url = list(
-      State_Assessment = "https://centerforassessment.github.io/SGPdata/",
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      State_Assessment = "www.cde.state.co.us/assessment/cmas",
+      College_Entrance = "www.cde.state.co.us/assessment/sat-psat",
+      ELP_Assessment = "www.cde.state.co.us/assessment/ela"
     ),
     grades = list(
       State_Assessment = as.character(3:8),
-      College_Entrance = c(),
-      ELP_Assessment = c(),
-      Interim_Assessment = c()
+      College_Entrance = as.character(9:11),
+      ELP_Assessment = as.character(0:12)
     ),
     demographics = list(
       State_Assessment = c("ETHNICITY", "FREE_REDUCED_LUNCH_STATUS", "ELL_STATUS", "IEP_STATUS", "GENDER"),
-      College_Entrance = c(),
-      ELP_Assessment  =  c(),
-      Interim_Assessment = c()
+      College_Entrance = c("ETHNICITY", "FREE_REDUCED_LUNCH_STATUS", "ELL_STATUS", "IEP_STATUS", "GENDER"),
+      ELP_Assessment  =  c("ETHNICITY", "FREE_REDUCED_LUNCH_STATUS", "ELL_STATUS", "IEP_STATUS", "GENDER")
     ),
     gof.path = list(
       State_Assessment = file.path("..", "Goodness_of_Fit"),
-      College_Entrance = c(),
-      ELP_Assessment  =  c(),
-      Interim_Assessment = c()
+      College_Entrance = file.path("..", "Goodness_of_Fit"),
+      ELP_Assessment  =  file.path("..", "..", "ACCESS", "Goodness_of_Fit")
     )
   )
 )
+
 
 ##   The following script will merge the report.config (universal) and custom.config lists and return 'report.config' to be used in next steps
 source(file.path(universal.content.path, "Learning_Loss_Analysis", "Meta_Data", "Report_Configs.R"))
